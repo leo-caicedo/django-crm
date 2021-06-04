6b62b5dcd3e19c44e0542b7abb026696779809c1
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-04 12:41:53 -0500

    inline Formsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 # Django
 from django.shortcuts import render, redirect
+from django.forms import inlineformset_factory
 
 # Models
 from .models import *
@@ -9,82 +10,86 @@ from .forms import OrderForm
 
 
 def home(request):
-	orders = Order.objects.all()
-	customers = Customer.objects.all()
-	total_customers = customers.count()
-	total_orders = orders.count()
-	delivered = orders.filter(status='Delivered').count()
-	pending = orders.filter(status='Pending').count()
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
 
-	context = {
-		'orders': orders,
-		'customers': customers,
-		'total_customers': total_customers,
-		'total_orders': total_orders,
-		'delivered': delivered,
-		'pending': pending,
-	}
+    context = {
+	'orders': orders,
+	'customers': customers,
+	'total_customers': total_customers,
+	'total_orders': total_orders,
+	'delivered': delivered,
+	'pending': pending,
+    }
 
-	return render(request, 'accounts/dashboard.html', context)
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-	products = Product.objects.all()
+    products = Product.objects.all()
 
-	return render(request, 'accounts/products.html', {'products':products})
+    return render(request, 'accounts/products.html', {'products':products})
 
 
 def customer(request, pk):
-	customer = Customer.objects.get(id=pk)
-	orders = customer.order_set.all()
-	order_count = orders.count()
-	context = {
-		'customer': customer,
-		'orders': orders,
-		'order_count': order_count,
-	}
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {
+	'customer': customer,
+	'orders': orders,
+	'order_count': order_count,
+    }
 
-	return render(request, 'accounts/customer.html', context)
+    return render(request, 'accounts/customer.html', context)
 
 
-def create_order(request):
-	form = OrderForm()
-	if request.method == 'POST':
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+def create_order(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
+    customer = Customer.objects.get(id=pk)
+    #form = OrderForm(initial={'customer': customer})
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    if request.method == 'POST':
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
 
-	context = {
-		'form': form,
-	}
+    context = {
+	'formset': formset,
+    }
 
-	return render(request, 'accounts/order_form.html', context)
+    return render(request, 'accounts/order_form.html', context)
 
 
 def update_order(request, pk):
-	order = Order.objects.get(id=pk)
-	form = OrderForm(instance=order)
-	if request.method == 'POST':
-		form = OrderForm(request.POST, instance=order)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
-	context = {
-		'form': form,
-	}
+    context = {
+            'form': form,
+    }
 
-	return render(request, 'accounts/order_form.html', context)
+    return render(request, 'accounts/order_form.html', context)
 
 
 def delete_order(request, pk):
-	order = Order.objects.get(id=pk)
-	if request.method == 'POST':
-		order.delete()
-		return redirect('/')
-	context = {
-		'order': order,
-	}
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {
+	'order': order,
+    }
 
-	return render(request, 'accounts/delete.html', context)
+    return render(request, 'accounts/delete.html', context)
