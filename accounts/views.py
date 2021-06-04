cf37a856c3f0225abb856d7445858e85de33f62e
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-06-04 15:54:23 -0500

    filter form table search

--- accounts/views.py
+++ accounts/views.py
@@ -5,91 +5,83 @@ from django.forms import inlineformset_factory
 # Models
 from .models import *
 
+# Filters
+from .filters import OrderFilter
+
 # Forms
 from .forms import OrderForm
 
 
 def home(request):
-    orders = Order.objects.all()
-    customers = Customer.objects.all()
-    total_customers = customers.count()
-    total_orders = orders.count()
-    delivered = orders.filter(status='Delivered').count()
-    pending = orders.filter(status='Pending').count()
+	orders = Order.objects.all()
+	customers = Customer.objects.all()
 
-    context = {
-	'orders': orders,
-	'customers': customers,
-	'total_customers': total_customers,
-	'total_orders': total_orders,
-	'delivered': delivered,
-	'pending': pending,
-    }
+	total_customers = customers.count()
 
-    return render(request, 'accounts/dashboard.html', context)
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
 
+	context = {'orders':orders, 'customers':customers,
+	'total_orders':total_orders,'delivered':delivered,
+	'pending':pending }
+
+	return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    products = Product.objects.all()
+	products = Product.objects.all()
 
-    return render(request, 'accounts/products.html', {'products':products})
-
+	return render(request, 'accounts/products.html', {'products':products})
 
 def customer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    orders = customer.order_set.all()
-    order_count = orders.count()
-    context = {
-	'customer': customer,
-	'orders': orders,
-	'order_count': order_count,
-    }
+	customer = Customer.objects.get(id=pk)
 
-    return render(request, 'accounts/customer.html', context)
+	orders = customer.order_set.all()
+	order_count = orders.count()
+
+	my_filter = OrderFilter(request.GET, queryset=orders)
+	orders = my_filter.qs 
+
+	context = {'customer':customer, 'orders':orders, 'order_count':order_count,
+	'my_filter':my_filter}
+	return render(request, 'accounts/customer.html',context)
 
 
 def create_order(request, pk):
-    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
-    customer = Customer.objects.get(id=pk)
-    #form = OrderForm(initial={'customer': customer})
-    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
-    if request.method == 'POST':
-        #form = OrderForm(request.POST)
-        formset = OrderFormSet(request.POST, instance=customer)
-        if formset.is_valid():
-            formset.save()
-            return redirect('/')
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	#form = OrderForm(initial={'customer':customer})
+	if request.method == 'POST':
+		#print('Printing POST:', request.POST)
+		#form = OrderForm(request.POST)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
+			return redirect('/')
 
-    context = {
-	'formset': formset,
-    }
-
-    return render(request, 'accounts/order_form.html', context)
-
+	context = {'form':formset}
+	return render(request, 'accounts/order_form.html', context)
 
 def update_order(request, pk):
-    order = Order.objects.get(id=pk)
-    form = OrderForm(instance=order)
-    if request.method == 'POST':
-        form = OrderForm(request.POST, instance=order)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
 
-    context = {
-            'form': form,
-    }
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
 
-    return render(request, 'accounts/order_form.html', context)
+	if request.method == 'POST':
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
 
+	context = {'form':form}
+	return render(request, 'accounts/order_form.html', context)
 
 def delete_order(request, pk):
-    order = Order.objects.get(id=pk)
-    if request.method == 'POST':
-        order.delete()
-        return redirect('/')
-    context = {
-	'order': order,
-    }
+	order = Order.objects.get(id=pk)
+	if request.method == "POST":
+		order.delete()
+		return redirect('/')
 
-    return render(request, 'accounts/delete.html', context)
+	context = {'item':order}
+	return render(request, 'accounts/delete.html', context)
--- crm/settings.py
+++ crm/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # app django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',    
+
+    # local apps
     'accounts',
 ]
 
